30565e3e52c569a9603c5894b4570526461d0669
mggb <mathieu.gelbon@hetic.net>
2018-04-27 12:36:36 +0200

    'h'

---
+++ index.php
@@ -0,0 +1,34 @@
+<?php
+require_once 'header.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="/css/reset.css">
+  <link rel="stylesheet" href="/css/style.css">
+  <title>Amandine Mouette</title>
+</head>
+<body>
+  <nav class="NavBar desktop">
+    <p class="NavBar-Contact">
+      Contact
+    </p>
+    <p class="NavBar-works">
+      Works
+    </p>
+    <p class="NavBar-About">
+      About
+    </p>
+  </nav>
+  <header class="Header-section">
+    <div>
+      <h1>Amndine mouette</h1>
+      <h2>Creative thinker & young developper</h2>
+    </div>
+  </header>
+</body>
+</html>
---
+++ integration/header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="/css/reset.css">
+  <link rel="stylesheet" href="/css/style.css">
+  <title>Amandine Mouette</title>
+</head>
+<body>
+  <header>
+    <div class="container">
+      <img src="logo-amandine.jpg" alt="logo portfolio amandine" title="logo portfolio">
+    </div>
+  </header>
+</body>
+</html>
